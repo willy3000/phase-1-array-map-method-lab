860d3c64899cfe13fc0b191080e6355d2db604b4
Jillo Wario <willywario0@gmail.com>
2023-12-20 09:04:07 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  const newTutorials = [];
+  tutorials.map((tutorial) => {
+    const wordArray = tutorial.split(" ");
+    const newWordArray = wordArray.map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    const newTutorial = newWordArray.join(" ");
+    newTutorials.push(newTutorial);
+  });
+  return newTutorials
+};
+
